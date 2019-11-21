229f875750103e0bcb499329992f38f690044e96
君塚史高 <f.kimizuka@gmail.com>
2019-11-21 11:59:34 +0900

    :)

---
+++ app.js
@@ -0,0 +1,13 @@
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const options = {
+  key: fs.readFileSync(path.resolve(__dirname, './https/server.key')),
+  cert: fs.readFileSync(path.resolve(__dirname, './https/server.crt'))
+};
+
+app.use("/", express.static(__dirname + "/public"));
+
+const server = https.createServer(options, app).listen(3000);
